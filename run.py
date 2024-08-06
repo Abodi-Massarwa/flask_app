2efb26a3a05a260aef9b97118fc7fd89b8eab3be
Abodi Massarwa <abodi.msarwi@gmail.com>
2024-08-06 16:04:10 +0300

    update port finding mechanism

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from flask_app import app
 import gspread
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True,port=6969)
+    port = int(os.getenv('PORT', 0))
+    app.run(debug=True,port=port)
     # account=gspread.service_account('/home/abodi-massarwa/website_final_project/secret_files/credentials.json')
     # sheet=account.open_by_url('https://docs.google.com/spreadsheets/d/1fs_FeivOv-LXOQ89OIug1PVZw1X3LqE7Dul0t8JsaDw/edit?gid=0#gid=0')
     # print(sheet.worksheet('Players&Valuations').get_all_records())# TODO success !!
